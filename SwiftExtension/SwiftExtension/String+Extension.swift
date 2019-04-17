0e1f56794779d738a2173c84c2508b05ceceb260
diep nguyen <>
2019-04-17 16:09:01 +0700

    Initial commit

---
+++ SwiftExtension/SwiftExtension/String+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  String+Extension.swift
+//  SwiftExtension
+//
+//  Created by Diep Nguyen on 4/17/19.
+//  Copyright © 2019 Diep Nguyen. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+}
---
+++ SwiftExtension/SwiftExtension/SwiftExtension.h
@@ -0,0 +1,19 @@
+//
+//  SwiftExtension.h
+//  SwiftExtension
+//
+//  Created by Diep Nguyen on 4/17/19.
+//  Copyright © 2019 Diep Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftExtension.
+FOUNDATION_EXPORT double SwiftExtensionVersionNumber;
+
+//! Project version string for SwiftExtension.
+FOUNDATION_EXPORT const unsigned char SwiftExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExtension/PublicHeader.h>
+
+
